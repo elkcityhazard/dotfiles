b2db6a292c1652c59c525a3a947887c5118f1df4
Andrew M McCall <andrew.m.mccall@gmail.com>
2024-03-10 15:54:58 -0400

    check for wsl env

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -1,3 +1,13 @@
+-- wsl and this plugin aren't getting along,
+-- don't setup bufferline if wsl is true
+
+local in_wsl = os.getenv('WSL_DISTRO_NAME') ~= nil
+
+
+if in_wsl then
+    return
+end
+
 require("bufferline").setup {
     options = {
         diagnostics = "nvim_lsp",
