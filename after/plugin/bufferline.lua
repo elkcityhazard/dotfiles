b703841d787d94b154e0d2fc54961870f00ab728
Andrew M McCall <andrew.m.mccall@gmail.com>
2025-03-23 20:36:05 -0400

    other stuff

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -1,6 +1,6 @@
 require("bufferline").setup({
 	options = {
-		diagnostics = "nvim_lsp",
+		diagnostics = { "nvim_lsp", "sql", "luasnip" },
 		diagnostics_indicator = function(count, level)
 			local icon = level:match("error") and " " or " "
 			return " " .. icon .. count
